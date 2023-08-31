ab88e34b327fc3eae844d7b5db0bcbfeb584a012
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-08-30 17:46:09 -0700

    modified based on authentication

--- screen/Header.js
+++ screen/Header.js
@@ -1,5 +1,5 @@
 'use client'
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import {Box, Text, Button, chakra, List, ListItem, Image} from "@chakra-ui/react"
 import {motion, AnimatePresence} from "framer-motion"
 import { useRouter } from 'next/router';
@@ -8,16 +8,24 @@ import { useSelector, useDispatch } from 'react-redux';
 import { worksActions } from '@/store/workslice';
 import Notification from './Notification';
 import { CloseIcon} from "@chakra-ui/icons"
+import { getAuthToken } from '@/util/authUtil';
 
 const Header = (props) => {
   // const [show, setShow] = useState(false);
+  const [auth, setAuth] = useState()
   const notification = useSelector(state => state.show.notification)
   const show = useSelector((state )=> state.show.show)
   const dispatch = useDispatch()
+  const getAuth = getAuthToken()
 
 const router = useRouter();
 
-  
+  useEffect(() => {
+    if (!getAuth) {
+      return
+    }
+    setAuth(getAuth)
+  }, [getAuth, setAuth])
 const showinMediaHandler = () => {
   if(router.pathname === "/media") {
     return true
@@ -26,6 +34,13 @@ const showinMediaHandler = () => {
 
 const showinMedia = showinMediaHandler()
 
+
+const logoutHandler = () => {
+  localStorage.removeItem("token")
+  setAuth(false)
+  router.push("/")
+};
+
 const adminHandler = () => {
   router.push("/authentication")
 };
@@ -33,13 +48,6 @@ const adminHandler = () => {
 const homeHandler = () => {
   router.push("/")
 };
-
-    const logo = {
-        color: '#8B6464', 
-        fontSize: "2rem",
-        fontWeight: '800', 
-        
-    }
 
   
     const showHandler = () => {
@@ -64,8 +72,10 @@ const homeHandler = () => {
                 <Link href="/#aboutus" style={{margin: "0 1.5rem 0 0", fontSize: "1.4rem", fontWeight: "600", color:"#000"}}>About us</Link>
                 <Link href="/#contact" style={{margin: "0 1.5rem", fontSize: "1.4rem", fontWeight: "600", color:"#000"}}>Contact</Link>
                 <Link href="/media" style={{margin: "0 1.5rem 0 0", fontSize: "1.4rem", fontWeight: "600", color:"#000"}}>Our works</Link>
+                {auth && <Link href="/upload" style={{margin: "0 1.5rem 0 0", fontSize: "1.4rem", fontWeight: "600", color:"#000"}}>Upload</Link>}
             </Box>}
-            <Button onClick={adminHandler} display={{base: "none", md: "block"}} cursor={"pointer"} background={"black"}  _hover={{ bg: 'gainsboro', color: "black" }} boxShadow={"0px 2px 7px 0px rgba(5, 50, 131, 0.52)"} color={"rgba(255, 255, 255, 1)"} padding={{base: "0.4rem 1rem", md: "0.7rem 3rem"}} borderRadius={"0.25rem"}>Admin</Button>
+            {!auth && <Button onClick={adminHandler} display={{base: "none", md: "block"}} cursor={"pointer"} background={"black"}  _hover={{ bg: 'gainsboro', color: "black" }} boxShadow={"0px 2px 7px 0px rgba(5, 50, 131, 0.52)"} color={"rgba(255, 255, 255, 1)"} padding={{base: "0.4rem 1rem", md: "0.7rem 3rem"}} borderRadius={"0.25rem"}>Admin</Button>}
+            {auth && <Button onClick={logoutHandler} display={{base: "none", md: "block"}} cursor={"pointer"} background={"black"}  _hover={{ bg: 'gainsboro', color: "black" }} boxShadow={"0px 2px 7px 0px rgba(5, 50, 131, 0.52)"} color={"rgba(255, 255, 255, 1)"} padding={{base: "0.4rem 1rem", md: "0.7rem 3rem"}} borderRadius={"0.25rem"}>Logout</Button>}
             {show && <List as={motion.ul} boxShadow={"0px 2px 7px 0px rgba(5, 50, 131, 0.52)"} 
              initial= {{opacity: 0, x: 15}}
              animate={{opacity: 1, x: 0}}
@@ -75,7 +85,9 @@ const homeHandler = () => {
               <ListItem borderBottom={"1px solid"} p={"0.5rem 2rem"}><Link href={"/"}>Home</Link></ListItem>
               <ListItem borderBottom={"1px solid"} p={"0.5rem 2rem"}><Link href={"/media"}>Our Works</Link></ListItem>
               <ListItem borderBottom={"1px solid"} p={"0.5rem 2rem"}><Link href={"/#contact"}>Contact Us</Link></ListItem>
-              <ListItem  p={"0.5rem 2rem"}><Link href={"/authentication"}>Admin</Link></ListItem>
+              {auth && <ListItem borderBottom={"1px solid"}  p={"0.5rem 2rem"}><Link href={"/upload"}>Upload</Link></ListItem>}
+              {!auth && <ListItem  p={"0.5rem 2rem"}><Link href={"/authentication"}>Admin</Link></ListItem>}
+              {auth && <ListItem onClick={logoutHandler} p={"0.5rem 2rem"}>Logout</ListItem>}
             </List>}
             {!show && <Image onClick={showHandler} display={{base: "block", md: "none"}} src="/images/icons/Menu.png" alt="hamburgermenu" width={39} height={50} />}
         
