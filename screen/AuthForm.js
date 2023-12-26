0995d8031c587257230f5d86a1e7f3abfd121092
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-12-27 00:46:58 +0100

    authform

--- screen/AuthForm.js
+++ screen/AuthForm.js
@@ -2,6 +2,7 @@
 
 import { worksActions } from '@/store/workslice';
 import { FormControl, FormLabel, Input, Box, Heading, Button, Stack } from '@chakra-ui/react'
+import { AnimatePresence, motion } from 'framer-motion';
 import { useRouter } from 'next/router';
 import React, {useEffect} from 'react';
 import { useForm} from "react-hook-form";
@@ -78,41 +79,41 @@ const AuthForm = () => {
   }, [isSubmitSuccessful, reset])
   
   return (
-    <Box width={"90%"} maxW={"45rem"} margin={"5rem auto"}>
-        <Heading lineHeight={"138%"} textAlign={"center"} color={"#000"} fontSize={{base: "1.3rem", md:"1.9rem"}} fontWeight={"600"}>Enter admin Credentials</Heading>
-        <form noValidate onSubmit={handleSubmit(submitAuthenticationHandler)}>
-            <Stack spacing={3} display={"flex"} flexDir={"column"} alignItems={"center"} justifyContent={"center"}>
-            <FormControl display={"flex"} flexDir={"column"} width={"100%"} marginTop={"1.7rem"}>
-                <FormLabel htmlFor='username'>User name</FormLabel>
-                <Input  type="text" id="username" placeholder="Enter Admin ID" autoComplete="username" 
-                {...register("username", {
-                  required: {
-                    value: true,
-                    message: "Enter a valid Admin Id!"
-                  }
-                })} 
-                />
-                {errors &&<p style={{color: "red"}}>{errors.username?.message}</p>}
-            </FormControl>
+        <Box as={motion.div} initial={{y: 100, opacity: 0}} animate={{y: -30, opacity: 1}} exit={{y: 100, opacity: 0}} transition={{delay: 0.55, duration:2}} width={"90%"} maxW={"45rem"} margin={"5rem auto"}>
+            <Heading lineHeight={"138%"} textAlign={"center"} color={"#000"} fontSize={{base: "1.3rem", md:"1.9rem"}} fontWeight={"600"}>Enter admin Credentials</Heading>
+            <form noValidate onSubmit={handleSubmit(submitAuthenticationHandler)}>
+                <Stack spacing={3} display={"flex"} flexDir={"column"} alignItems={"center"} justifyContent={"center"}>
+                <FormControl display={"flex"} flexDir={"column"} width={"100%"} marginTop={"1.7rem"}>
+                    <FormLabel htmlFor='username'>User name</FormLabel>
+                    <Input  type="text" id="username" placeholder="Enter Admin ID" autoComplete="username" 
+                    {...register("username", {
+                      required: {
+                        value: true,
+                        message: "Enter a valid Admin Id!"
+                      }
+                    })} 
+                    />
+                    {errors &&<p style={{color: "red"}}>{errors.username?.message}</p>}
+                </FormControl>
 
-            <FormControl display={"flex"} flexDir={"column"} width={"100%"} marginTop={"1.7rem"}>
-                <FormLabel htmlFor='password'>Password</FormLabel>
-                <Input  type="password" id="password" placeholder="Password" autoComplete="password"
-                  {...register("password", {
-                    required: {
-                      value: true,
-                      message: "Enter a valid admin Password!"
-                    },
-                    validate: (fieldValue) => fieldValue.trim().length > 5 || "Password must greater than 5!"
-                  })}
-                />
-                {errors && <p style={{color: "red"}}>{errors.password?.message}</p>}
-            </FormControl>
-            <Button type='submit' cursor={"pointer"} background={"black"}  _hover={{ bg: 'gainsboro', color: "black" }} boxShadow={"0px 2px 7px 0px rgba(5, 50, 131, 0.52)"} color={"rgba(255, 255, 255, 1)"} padding={{base: "0.4rem 1rem", md: "0.7rem 3rem"}} borderRadius={"0.25rem"}>Login</Button>
-            </Stack>
-        </form>
-    </Box>
+                <FormControl display={"flex"} flexDir={"column"} width={"100%"} marginTop={"1.7rem"}>
+                    <FormLabel htmlFor='password'>Password</FormLabel>
+                    <Input  type="password" id="password" placeholder="Password" autoComplete="password"
+                      {...register("password", {
+                        required: {
+                          value: true,
+                          message: "Enter a valid admin Password!"
+                        },
+                        validate: (fieldValue) => fieldValue.trim().length > 5 || "Password must greater than 5!"
+                      })}
+                    />
+                    {errors && <p style={{color: "red"}}>{errors.password?.message}</p>}
+                </FormControl>
+                <Button type='submit' cursor={"pointer"} background={"black"}  _hover={{ bg: 'gainsboro', color: "black" }} boxShadow={"0px 2px 7px 0px rgba(5, 50, 131, 0.52)"} color={"rgba(255, 255, 255, 1)"} padding={{base: "0.4rem 1rem", md: "0.7rem 3rem"}} borderRadius={"0.25rem"}>Login</Button>
+                </Stack>
+            </form>
+        </Box>
   )
 }
 
-export default React.memo(AuthForm)
+export default AuthForm
